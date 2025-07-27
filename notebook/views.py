3b1a7caee4c7fe5444dff2dae46337bfcda644e4
unknown <18163030298@163.com>
2025-07-27 18:04:24 +0800

    2

--- notebook/views.py
+++ notebook/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Topic
+from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
 
 
@@ -47,5 +47,18 @@ def new_entry(request,topic_id):
     context = {"form":form,"topic":topic}
     return render(request,"notebook/new_entry.html",context)
 
+def edit_entry(request,entry_id):
+    entry=Entry.objects.get(id=entry_id)
+    topic=entry.topic
+
+    if request.method !="POST":
+        form=EntryForm(instance=entry)
+    else:
+        form=EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("notebook:topic",topic_id=topic.id)
+    context={"entry":entry,"topic":topic,"form":form}
+    return render(request,"notebook/edit_entry.html",context)
 
 
