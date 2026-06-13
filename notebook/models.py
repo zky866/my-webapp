b7405a0f73442451a2a2dcb37b53f0f9f8e8996e
unknown <18163030298@163.com>
2026-06-13 20:44:59 +0800

    modol

--- notebook/models.py
+++ notebook/models.py
@@ -20,4 +20,6 @@ class Entry(models.Model):
         verbose_name_plural="entries"
 
     def __str__(self):
-        return f"{self.text[:50]}......"
+        if len(self.text) > 50:
+            return f"{self.text[:50]}......"
+        return self.text
